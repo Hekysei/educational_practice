c552da529a066546ca8952727aed9c025b30b259
Hekysei <150318238+Hekysei@users.noreply.github.com>
2026-06-22 19:34:33 +0800

    Добавлен поиск всех точек контура

--- task11/main.py
+++ task11/main.py
@@ -6,6 +6,13 @@ hsv_image = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2HSV)
 
 green_mask = cv2.inRange(hsv_image, (36, 25, 25), (70, 255, 255))
 
-cv2.imshow("mask", green_mask)
+contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+for contour in contours:
+    for dot in contour:
+        print(dot)
+        cv2.circle(image, dot[0], 7, (0, 0, 255), -1)
+
+cv2.imshow("res", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
